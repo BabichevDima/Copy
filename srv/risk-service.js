a5d8c0ee80bf4e20b86d8e01e9a903d379690d6c
Dima Babichau <dzmitry.babichau@leverx.com>
2024-05-30 13:27:30 +0000

    feat: get Mitigations from Risks

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -6,13 +6,17 @@ const cds = require('@sap/cds')
 module.exports = cds.service.impl(async function() {
 
     // Define constants for the Risk and BusinessPartner entities from the risk-service.cds file
-    const { Risks, BusinessPartners } = this.entities;
+    const { Risks, Mitigations, BusinessPartners } = this.entities;
 
     // This handler will be executed directly AFTER a READ operation on RISKS
     // With this we can loop through the received data set and manipulate the single risk entries
-    this.after("READ", Risks, (data) => {
+    this.after("READ", Risks, async (data) => {
         // Convert to array, if it's only a single risk, so that the code won't break here
         const risks = Array.isArray(data) ? data : [data];
+
+        // Get all the mitigations
+        const aMitigations = await SELECT.from(Mitigations)
+        console.log(aMitigations)
 
         // Looping through the array of risks to set the virtual field 'criticality' that you defined in the schema
         risks.forEach((risk) => {
